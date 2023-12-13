2a33a0bf73272b96ab2d6a0faa0a6797b361791e
Hay I'am Inces <fitringer612@gmail.com>
2023-12-13 14:29:03 +0700

    testttt url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'localhost',
+      'placeimg.com',
+      'smn.newfemme.co',
+      'newfemme.co',
+      'bitter.bragherstudio.com',
+      'lh3.googleusercontent.com',
+      'api.newfemme.co',
+      'smn.newfemme.co',
+      'upload.wikimedia.org',
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
